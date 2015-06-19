50b0398f36421c4c1b7363e816054cc786d570ff
Qiu Jieting-JQIU <jqiu@jqiu-mac01.mot.com>
2015-06-19 11:52:01 -0700

    more widegets and landscape view

--- app/src/main/java/com/example/jqiu/criminalintent/Crime.java
+++ app/src/main/java/com/example/jqiu/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.jqiu.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 /**
  * Created by jqiu on 6/18/15.
@@ -7,9 +8,29 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate( Date date ){
+        mDate = date;
     }
 
     public String getTitle() {
