5677c6edf139e5f922d7d92e9b6180e4c50b26fc
Qiu Jieting-JQIU <jqiu@jqiu-mac01.mot.com>
2015-06-19 10:40:43 -0700

    layout

--- app/src/main/java/com/example/jqiu/criminalintent/Crime.java
+++ app/src/main/java/com/example/jqiu/criminalintent/Crime.java
@@ -1,7 +1,28 @@
 package com.example.jqiu.criminalintent;
 
+import java.util.UUID;
 /**
  * Created by jqiu on 6/18/15.
  */
 public class Crime {
+    private UUID mId;
+    private String mTitle;
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+
+    public UUID getId() {
+        return mId;
+    }
+
 }
--- app/src/main/java/com/example/jqiu/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/jqiu/criminalintent/CrimeActivity.java
@@ -1,38 +1,29 @@
 package com.example.jqiu.criminalintent;
 
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class CrimeActivity extends Activity {
+public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_crime, menu);
-        return true;
-    }
+        FragmentManager fm = getSupportFragmentManager();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
